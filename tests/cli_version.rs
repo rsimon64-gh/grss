817a8cb535bc0668b34c94ecd8f31b2c8aebcc27
root <rsimon64@web.de>
2025-02-09 23:50:24 +0100

    adding tests and license text

--- tests/cli_version.rs
+++ tests/cli_version.rs
@@ -9,7 +9,7 @@ fn version_flag_works() -> Result<(), Box<dyn Error>> {
     cmd.arg("--version");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("grss_clone 0.4.4"));
+        .stdout(predicate::str::contains("grss_clone 0.5.0"));
 
     Ok(())
 }
